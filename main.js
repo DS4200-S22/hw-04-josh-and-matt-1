e20858248d079f6449e0143a0197a72fa52285ed
JoshuaJKo <ko.j@northeastern.edu>
2022-02-16 15:21:42 -0500

    added functions
    Need to figure out how to add event listener

--- main.js
+++ main.js
@@ -1,41 +1,39 @@
-let timesPressed = 0;
+let timesPressed = 2000;
 
+// circle1: Add text in the center of the circle that shows how many times
+//  this svg has been clicked on. Each time the svg is clicked on, the count should update.
 function shapeOneClicked() {
-    //console.log("Button 1 was clicked"); 
-
-    // Generate random number
-    let rand_num = Math.random(); 
-
-    // Set innerHTML of div
-    document.getElementById("random_num").innerHTML = rand_num; 
+    console.log(timesPressed);
+    timesPressed ++;
+    document.getElementById("t").innerHTML = timesPressed; 
 }
 
-//Students to add button2Clicked code 
+// circle2: When this svg is mousedover a bold black border should show. On mouseout,
+//  the bold black border should disappear.
 function shapeTwoClicked() {
-    //console.log("Button 2 was clicked"); 
+    console.log('Shape Two Clicked');
+}
 
+// circle3: Add functionality such that clicking on this svg hides it from the page. 
+// Clicking on the blank space the svg was in should un-hide the svg from the page. 
+// Hint: think about setting opacity in your css to show or hide the circle.
+function shapeThreeClicked() {
+    console.log('Shape Three Clicked');
     // Get svg class
-    let curr_color = document.getElementById("c").getAttribute("class"); 
+    let curr_color = document.getElementById("cThree").getAttribute("class"); 
 
     // Change color 
-    if (curr_color == "blue") {
-        document.getElementById("c").setAttribute("class", "green"); 
+    if (curr_color == "red") {
+        document.getElementById("cThree").setAttribute("class", "opaque"); 
     } else {
-        document.getElementById("c").setAttribute("class", "blue"); 
+        document.getElementById("cThree").setAttribute("class", "red"); 
     }
 
 }
 
-function shapeTwoClicked() {
-    //console.log("Button 2 was clicked"); 
-
-    // Get svg class
-    let curr_color = document.getElementById("c").getAttribute("class"); 
-
-    // Change color 
-    if (curr_color == "blue") {
-        document.getElementById("c").setAttribute("class", "green"); 
-    } else {
-        document.getElementById("c").setAttribute("class", "blue"); 
-    }
+function addShapeEventListeners() {
+    console.log('does this load?')
+    document.getElementById('cOne').addEventListener('click', shapeOneClicked());
+    document.getElementById('cTwo').addEventListener('mouseover', shapeTwoClicked());
+    document.getElementById('cThree').addEventListener('click', shapeThreeClicked());
 }
